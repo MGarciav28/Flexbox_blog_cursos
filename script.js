c907fcbedd86f29a45cee5d20a8be676c340bbf2
MGarciav28 <mgarciavilchis@gmail.com>
2021-11-06 02:59:29 -0600

    Se agrega alerta sobre funcionalidades de menu principal

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var articulos = document.getElementsByClassName('article');
-var enlaces = document.getElementsByClassName('link');
+var enlaces = document.getElementsByClassName('news-card');
+
 
 for(let i=0; i<=enlaces.length-1; i++){
     enlaces[i].addEventListener('click', cambio);
 }
+
+
 
 function cambio(){
 var valor = this.id;
@@ -18,4 +21,8 @@ function borrar(indice){
             articulos[i].classList.remove('hidden');
         }
     }
+}
+
+function aviso(){
+    alert("Esta pagína es una version preliminar con simulación de fuincionalidades, los botones Blog y Contacto son solo ilustrativos y sus funcionalidades se añadiran posteriormente");
 }
